985d2c30a90c05ad17595c42bc415838ebd100ec
鈴木希海 <121pg0014@amg.tokyo.ac.jp>
2021-12-15 00:57:42 +0900

    四次元ベクトルを表すVector4クラスを実装

---
+++ Vector4.cpp
@@ -0,0 +1,112 @@
+/*****************************************************************//**
+ * @file   Vector4.cpp
+ * @brief  四次元ベクトルクラスの定義
+ * 
+ * @author 鈴木希海
+ * @date   December 2021
+ *********************************************************************/
+#include "Vector4.h"
+#include <cmath>
+
+namespace AppFrame {
+  namespace Math {
+
+    Vector4::Vector4() : _x(0), _y(0), _z(0), _w(1.0) {
+    }
+
+    Vector4::Vector4(float x, float y, float w, float z) {
+      _x = x;
+      _y = y;
+      _z = z;
+      _w = w;
+    }
+
+    void Vector4::Add(const Vector4 vector) {
+      _x += vector._x;
+      _y += vector._y;
+      _z += vector._z;
+    }
+
+    void Vector4::Add(const float x, const float y, const float z) {
+      _x += x;
+      _y += y;
+      _z += z;
+    }
+
+    void Vector4::Sub(const Vector4 vector) {
+      _x -= vector._x;
+      _y -= vector._y;
+      _x -= vector._z;
+    }
+
+    void Vector4::Sub(const float x, const float y, const float z) {
+      _x -= x;
+      _y -= y;
+      _z -= z;
+    }
+
+    Vector4 Vector4::operator+(const Vector4 vector) {
+      auto x = _x + vector._x;
+      auto y = _y + vector._y;
+      auto z = _z + vector._z;
+      return Vector4(x, y, z);
+    }
+
+    Vector4 Vector4::operator-(const Vector4 vector) {
+      auto x = _x - vector._x;
+      auto y = _y - vector._y;
+      auto z = _z - vector._z;
+      return Vector4(x, y, z);
+    }
+
+    Vector4 Vector4::operator*(const float scalar) {
+      auto x = _x * scalar;
+      auto y = _y * scalar;
+      auto z = _z * scalar;
+      return Vector4(x, y, z);
+    }
+
+    Vector4 Vector4::operator/(const float scalar) {
+      auto x = _x / scalar;
+      auto y = _y / scalar;
+      auto z = _z / scalar;
+      return Vector4(x, y, z);
+    }
+
+    float Vector4::Length() const {
+      return std::sqrt(_x + _x * _y + _y * _z + _z);
+    }
+
+    void Vector4::Normalize() {
+      auto length = Length();
+      _x /= length;
+      _y /= length;
+      _z /= length;
+    }
+
+    Vector4 Vector4::Normalize() const {
+      auto length = Length();
+      auto x = _x / length;
+      auto y = _y / length;
+      auto z = _z / length;
+      return Vector4(x, y, z);
+    }
+
+    float Vector4::Dot(const Vector4 vector) const {
+      return _x * vector._x + _y * vector._y + _z * vector._z;
+    }
+
+    Vector4 Vector4::Cross(const Vector4 vector) const {
+      auto x = _y * vector._y - vector._y;
+      auto y = _z * vector._z - vector._z;
+      auto z = _x * vector._x - vector._x;
+      return Vector4(x, y, z);
+    }
+
+    void Vector4::Fill(const float value) {
+      _x = value;
+      _y = value;
+      _z = value;
+    }
+  }
+}
---
+++ Vector4.h
@@ -0,0 +1,176 @@
+/*****************************************************************//**
+ * @file   Vector4.h
+ * @brief  四次元ベクトルクラス
+ * 
+ * @author 鈴木希海
+ * @date   December 2021
+ *********************************************************************/
+#pragma once
+#include <tuple>
+
+namespace AppFrame {
+  namespace Math {
+    /**
+     * @class Vector4
+     * @brief 四次元ベクトル
+     */
+    class Vector4 {
+    public:
+      /**
+       * @brief 空ベクトルの生成
+       */
+      Vector4();
+      /**
+       * @brief ベクトルの生成
+       * @param x x成分
+       * @param y y成分
+       * @param z z成分
+       * @param w w成分(デフォルトでは1.0)
+       */
+      Vector4(float x, float y, float z, float w = 1.0);
+      /**
+       * @brief ベクトル同士の加算
+       * @param vector ベクトル(加数)
+       */
+      void Add(const Vector4 vector);
+      /**
+       * @brief ベクトルの加算
+       * @param x x成分
+       * @param y y成分
+       * @param z z成分
+       */
+      void Add(const float x, const float y, const float z);
+      /**
+       * @brief ベクトル同士の減算
+       * @param vector ベクトル(減数)
+       */
+      void Sub(const Vector4 vector);
+      /**
+       * @brief ベクトルの減算
+       * @param x x成分
+       * @param y y成分
+       * @param z z成分
+       */
+      void Sub(const float x, const float y, const float z);
+      /**
+       * @brief  2次元ベクトルの取得
+       * @return xy成分を返す
+       */
+      const auto GetVector2() {
+        return std::make_pair(_x, _y);
+      }
+      /**
+       * @brief  3次元ベクトルの取得
+       * @return xyz成分を返す
+       */
+      const auto GetVector3() {
+        return std::make_tuple(_x, _y, _z);
+      }
+      /**
+       * @brief  4次元ベクトルの取得
+       * @return 
+       */
+      const auto GetVector4() {
+        return std::make_tuple(_x, _y, _z, _w);
+      }
+      /**
+       * @brief  x成分の取得
+       * @return x成分
+       */
+      const float GetX() {
+        return _x;
+      }
+      /**
+       * @brief  y成分の取得
+       * @return y成分
+       */
+      const float GetY() {
+        return _y;
+      }
+      /**
+       * @brief  z成分の取得
+       * @return z成分
+       */
+      const float GetZ() {
+        return _z;
+      }
+      /**
+       * @brief  w成分の取得
+       * @return w成分
+       */
+      const float GetW() {
+        return _w;
+      }
+      /**
+       * @brief  ベクトルの長さの取得
+       * @return ベクトルの長さ
+       */
+      float Length() const;
+      /**
+       * @brief  ベクトルの正規化
+       */
+      void Normalize();
+      /**
+       * @brief  単位ベクトルの取得
+       * @return 単位ベクトル
+       */
+      Vector4 Normalize() const;
+      /**
+       * @brief  ベクトル同士の内積
+       * @param  vector ベクトル
+       * @return 内積
+       */
+      float Dot(const Vector4 vecotr) const;
+      /**
+       * @brief  ベクトルの外積
+       * @param  vector ベクトル
+       * @return ベクトル
+       */
+      Vector4 Cross(const Vector4 vector) const;
+      /**
+       * @brief  指定した値をxyz成分に代入する
+       * @param  value 値
+       */
+      void Fill(const float value);
+      /**
+       * @brief  ベクトル同士の加算
+       * @param  vector ベクトル(加数)
+       * @return 加算した4次元ベクトル
+       */
+      Vector4 operator + (const Vector4 vector);
+      /**
+       * @brief  ベクトル同士の減算
+       * @param  vector ベクトル(減数)
+       * @return 減算した4次元ベクトル
+       */
+      Vector4 operator - (const Vector4 vector) {
+        auto x = _x - vector._x;
+        auto y = _y - vector._y;
+        auto z = _z - vector._z;
+        return Vector4(x, y, z);
+      }
+      /**
+       * @brief  ベクトルのスカラー倍
+       * @param  scalar スカラー値
+       * @return スカラー倍した4次元ベクトル
+       */
+      Vector4 operator * (const float scalar) {
+        auto x = _x * scalar;
+        auto y = _y * scalar;
+        auto z = _z * scalar;
+        return Vector4(x, y, z);
+      }
+      /**
+       * @brief  ベクトルのスカラー割り
+       * @param  scalar スカラー値
+       * @return スカラー割りした4次元ベクトル
+       */
+      Vector4 operator/(const float scalar);
+    private:
+      float _x; //!< x成分
+      float _y; //!< y成分
+      float _z; //!< z成分
+      float _w; //!< w成分
+    };
+  } // namespace Math
+} // namespace AppFrame
