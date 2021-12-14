300703d08a0a95a1847739ee0375cf868287bb04
鈴木希海 <121pg0014@amg.tokyo.ac.jp>
2021-12-14 12:26:12 +0900

    ApplicationBaseクラス：アプリケーションの初期化処理を仮実装

--- source/Application/ApplicationBase.cpp
+++ source/Application/ApplicationBase.cpp
@@ -6,27 +6,62 @@
  * @date   December 2021
  *********************************************************************/
 #include "ApplicationBase.h"
+#include <string>
+#include <algorithm>
+#include <stdexcept>
+#include <DxLib.h>
+
+namespace {
+  constexpr auto WIDTH_MIN = 640;
+  constexpr auto HEIGHT_MIN = 480;
+
+  constexpr auto INIT_ERROR = -1; // 初期化失敗
+}
 
 namespace AppFrame {
   namespace App {
     
     ApplicationBase::ApplicationBase() {
-      // ウィンドウ情報の設定
-      auto&& [width, height, bit] = _window;
-      width = WINDOW_W;
-      height = WINDOW_H;
-      bit = WINDOW_B;
-      // 生成フラグの登録
-      if (!_instance) {
 #ifdef _DEBUG
-
+      _windowMode = false;
 #endif
-      }
-
+#ifndef _DEBUG
+      _windowMode = true;
+#endif
     }
 
     ApplicationBase::~ApplicationBase() {
+    }
 
+    bool ApplicationBase::Init() {
+      SetWindowSize(WINDOW_W, WINDOW_H);
+      ChangeWindowMode(true);
+      if (DxLib::DxLib_Init() == INIT_ERROR) {
+#ifdef _DEBUG
+        throw std::logic_error("ApplicationBase:DXライブラリの初期化に失敗しました\n");
+#endif
+        return false;
+      }
+      SetBackgroundColor(0, 0, 255);
+      SetDrawScreen(DX_SCREEN_BACK);
+      // Zバッファの設定
+      SetUseZBuffer3D(TRUE);
+      SetWriteZBuffer3D(TRUE);
+      return true;
+    }
+
+    void ApplicationBase::SetWindowSize(int width, int height, bool bit) {
+      // 解像度情報の設定
+      auto&& [sizeX, sizeY, colorBit] = _window;
+      sizeX = std::clamp(width, WIDTH_MIN, WINDOW_W);
+      sizeY = std::clamp(width, HEIGHT_MIN, WINDOW_H);
+      if (bit) {
+        colorBit = COLOR_BIT_32;
+      }
+      else {
+        colorBit = COLOR_BIT_16;
+      }
+      SetGraphMode(sizeX, sizeY, colorBit);
     }
   } // namespace App
 } // namespace AppFrame
--- source/Application/ApplicationBase.h
+++ source/Application/ApplicationBase.h
@@ -14,7 +14,8 @@
 namespace AppFrame {
   constexpr auto WINDOW_W = 1920; //!< デフォルトの解像度
   constexpr auto WINDOW_H = 1080; //!< デフォルトの解像度
-  constexpr auto WINDOW_B = 32;   //!< デフォルトのカラービット数
+  constexpr auto COLOR_BIT_32 = 32;  //!< デフォルトのカラービット数
+  constexpr auto COLOR_BIT_16 = 16;  //!<
   /** Application用名前空間 */
   namespace App {
     /**
@@ -33,6 +34,12 @@ namespace AppFrame {
        */
       ~ApplicationBase();
       /**
+       * @brief  アプリケーションの初期化
+       * @return 初期化に成功した場合はtrue
+       *         失敗した場合はfalseを返す
+       */
+      virtual bool Init();
+      /**
        * @brief  画面サイズの取得
        * @return 画面の縦幅と横幅を返す
        */
@@ -46,9 +53,18 @@ namespace AppFrame {
       inline auto GetWidnowData() {
         return _window;
       }
+      /**
+       * @brief  ウィンドウ情報の設定
+       * @param  width  解像度
+       * @param  height 解像度
+       * @param  bit    カラービット数を変更するかのフラグ
+       *                true()
+       */
+      static void SetWindowSize(int width, int height, bool bit = true);
     protected:
       static inline std::tuple<int, int, int> _window; //!< ウィンドウ情報
       static inline bool _isAdd = false; //!< 生成フラグ
+      static inline bool _windowMode;    //!< ウィンドウモード
       static inline std::shared_ptr<ApplicationBase> _instance; //!< 実態
     };
   }
