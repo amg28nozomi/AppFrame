ebfaef54b07fe101a333ac89ecc7239a725525c6
鈴木希海 <121pg0014@tokyo.amg.ac.jp>
2021-12-22 20:37:26 +0900

    jsonファイルの書き込み処理を行うWriteJsonクラスを仮追加(現状jsonファイルの作成処理のみ実装)

---
+++ source/FileServer/WriteJson.cpp
@@ -0,0 +1,35 @@
+/*****************************************************************//**
+ * @file   WriteJson.cpp
+ * @brief  jsonファイルの書き込み処理クラス
+ * 
+ * @author 鈴木希海
+ * @date   December 2021
+ *********************************************************************/
+#include "WriteJson.h"
+#include "FileBase.h"
+#include <fstream>
+#ifdef _DEBUG
+#include <stdexcept>
+#endif
+
+namespace AppFrame {
+  namespace FileServer {
+
+    std::filesystem::path WriteJson::AddJson(std::filesystem::path jsonName) {
+      // ファイルは既に生成されているか？
+      if (std::filesystem::exists(jsonName)) {
+        return jsonName; // 生成済み
+      }
+      if (jsonName.stem() == ".json") {
+        // 拡張子がjsonでない場合も弾く
+        throw std::logic_error("WriteJson::AddJson:対象ファイルの拡張子がjson形式ではありません\n");
+        return "";
+      }
+      // 存在していない場合はjsonファイルを作成
+      std::ofstream jsonFile;
+      jsonFile.open(jsonName.filename().string(), std::ios::out);
+      jsonFile.close();
+      return jsonName;
+    }
+  } // namespace FileServer
+} // namespace AppFrame
